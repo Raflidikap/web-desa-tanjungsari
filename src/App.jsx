0888ecfeae3067c518856ea783e6c0f71505313d
Melvin Marcello <melvinmarcello2978@gmail.com>
2023-03-27 14:23:18 +0700

    change title and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-import Header from "./components/Header"
-import Topografi from "./components/Topografi"
-import Carousel from "./components/Carousel"
-import Beranda from "./components/Beranda"
+import Beranda from "./pages/Beranda"
+import Header from './components/Header'
+import ProfileDesa from "./pages/Profile-Desa"
+import UMKM from "./pages/Umkm"
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Carousel/>
+      {/* <Carousel/>
       <Beranda/>
-      <Topografi/>
+      <Topografi/> */ }
+      <Header/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Beranda/>} />
+          <Route path="/profiledesa" element={<ProfileDesa/>} />
+          <Route path="/umkm" element={<UMKM/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
