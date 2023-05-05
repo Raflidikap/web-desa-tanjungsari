03498c60861c9dfdd2d85d3502340593dd8e8612
Melvin Marcello <melvinmarcello2978@gmail.com>
2023-05-05 17:49:40 +0700

    getpostlist

--- src/api.js
+++ src/api.js
@@ -1,6 +1,10 @@
 export const getUmkmList = async () =>{
-    const umkms = await fetch('https://joyous-pink-catfish.cyclic.app/umkm?page=1&limit=3')
+    const umkms = await fetch(`${process.env.REACT_APP_BASEURL}umkm?page=1&limit=3`)
     .then(res=>res.json())
-    console.log(umkms)
     return umkms
+}
+export const getPostList = async () =>{
+    const posts = await fetch(`${process.env.REACT_APP_BASEURL}posts?page=1&limit=3`)
+    .then(res=>res.json())
+    return posts
 }
